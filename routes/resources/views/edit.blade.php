24fa555b1fa7c61c4dcbffd82f1c4b35938ca1a4
Bruno Mourao de Lavor Moreira <brunolmourao10@gmail.com>
2018-05-04 16:49:29 -0300

    Criando novas tabelas e modificando views

---
+++ routes/resources/views/edit.blade.php
@@ -0,0 +1,70 @@
+<!-- edit.blade.php -->
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('title', 'Treinameto')
+
+@section('content_header')
+    <h1>Editar Pessoa</h1>
+@stop
+
+@section('content')
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+        <form method="post" action="{{action('PessoasController@update', $id)}}">
+        @csrf
+        <input name="_method" type="hidden" value="PATCH">
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="name">Nome:</label>
+            <input type="text" class="form-control" name="name" value="{{$pessoa->nomepessoa}}">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="email">Matricula</label>
+              <input type="text" class="form-control" name="matricula" value="{{$pessoa->matpessoas}}">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="email">Email</label>
+              <input type="text" class="form-control" name="email" value="{{$pessoa->email}}">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="number">Telefone Celular:</label>
+              <input type="text" class="form-control" name="number" value="{{$pessoa->celular}}">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4" style="margin-top:60px">
+            <button type="submit" class="btn btn-success" style="margin-left:38px">Update</button>
+            <a href="{{action('PessoasController@index')}}" class="btn btn-warning">Voltar</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
